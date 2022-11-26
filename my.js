3ef9aa9ee8851ed66dee23c82d11fac92e310eaf
nozipho97 <mayilanozipho@gmail.com>
2022-11-26 13:42:42 +0200

    msg

--- my.js
+++ my.js
@@ -1,19 +1,107 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
-buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.getElementsByClassName == "clear") {
-      display.innerText = "";
-    } else if (item.getElementsByClassName == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } else {
-      display.innerText += item.id;
-    }
-  };
+let screen = document.querySelector("#screen");
+let number1 = document.getElementById("number1");
+let subtract = document.getElementById("minus");
+let addition = document.getElementById("plus");
+let multiplication = document.getElementById("multiply");
+let division = document.getElementById("division");
+let decimal = document.getElementById("decimal");
+let deletes = document.getElementById("delete");
+let allowed = true;
+
+number1.addEventListener("click", () => {
+  let displays = number1.innerText;
+  screen.value += displays;
 });
+
+number2.addEventListener("click", () => {
+  let displayMinus = number2.innerText;
+  screen.value += displayMinus;
+});
+number0.addEventListener("click", () => {
+  let displayMinus = number0.innerText;
+  screen.value += displayMinus;
+});
+number3.addEventListener("click", () => {
+  let displays = number3.innerText;
+  screen.value += displays;
+});
+number4.addEventListener("click", () => {
+  let displays = number4.innerText;
+  screen.value += displays;
+});
+number5.addEventListener("click", () => {
+  let displays = number5.innerText;
+  screen.value += displays;
+});
+number6.addEventListener("click", () => {
+  let displays = number6.innerText;
+  screen.value += displays;
+});
+number7.addEventListener("click", () => {
+  let displays = number7.innerText;
+  screen.value += displays;
+});
+number8.addEventListener("click", () => {
+  let displays = number8.innerText;
+  screen.value += displays;
+});
+number9.addEventListener("click", () => {
+  let displays = number9.innerText;
+  screen.value += displays;
+});
+subtract.addEventListener("click", () => {
+  let displays = subtract.innerText;
+  screen.value += displays;
+  allowed = true;
+});
+addition.addEventListener("click", () => {
+  displays = addition.innerText;
+  screen.value += displays;
+  allowed = true;
+});
+multiplication.addEventListener("click", () => {
+  displays = multiplication.innerText;
+  screen.value += displays;
+  allowed = true;
+});
+division.addEventListener("click", () => {
+  displays = division.innerText;
+  screen.value += displays;
+  allowed = true;
+});
+// decimal.addEventListener("click", () => {
+//   displays = decimal.innerText;
+//   screen.value += displays;
+// })
+
+let equal = document.querySelector("#equal");
+equal.addEventListener("click", function () {
+  screen.value = eval(screen.value);
+  if (screen.value.includes(".")) {
+    allowed = false;
+  } else allowed = true;
+});
+let deleteBtn = document.querySelector("#delete");
+deleteBtn.addEventListener("click", function () {
+  if (screen.value[screen.value.length - 1] == ".") {
+    screen.value = screen.value.slice("0", "-1");
+    allowed = true;
+  } else {
+    screen.value = screen.value.slice("0", "-1");
+  }
+})
+decimal.addEventListener("click", function () {
+  // screen.value = decimal.innerText;
+  if (allowed) {
+    screen.value += decimal.innerText;
+    allowed = false;
+  }
+
+});
+
+let clearAll = document.getElementById("clearAll");
+clearAll.addEventListener("click", () => {
+  screen.value = "";
+  allowed = true;
+});
+
